aceb5b9980c7a3dc7817ebf2cf2ddd799f29b7f5
kumardevnotes <kumar.thoudishetti@gmail.com>
2023-10-13 22:27:22 +0530

    first java demo program

---
+++ src/test/java/FirstDemo.java
@@ -0,0 +1,34 @@
+
+/*
+ * To start with java, we need to create a class
+ * Find that a class is created with main method
+ * Write some sysout and then press ctrl + spacebar, it creates System.out.println()
+ * 
+ * */
+public class FirstDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Everyone!");
+		//calling a method in java
+		displayName();
+		displayStatus();
+	}
+	
+	//creating or declaring a user defined method in java
+	public static void displayName() {
+		System.out.println("John Legend");
+	}
+	
+	public static void displayStatus() {
+		int age = 10;
+		if(age >= 18) { // 10 >= 18 --> false
+			System.out.println("Its a major");
+		}
+		else {
+			System.out.println("Its a minor");
+		}
+	}
+	
+	//System.out.println("I am an error. I will not be shown on console");
+
+}
