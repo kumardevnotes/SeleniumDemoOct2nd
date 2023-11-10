0a2a2e6820da579f818e6daa1097cb1a32c5d52e
kumardevnotes <kumar.thoudishetti@gmail.com>
2023-11-10 23:07:29 +0530

    added

---
+++ src/test/java/com/selenium/demo/SeleniumXPathDemo.java
@@ -0,0 +1,37 @@
+package com.selenium.demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumXPathDemo {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = null;
+		driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.get("https://www.speaklanguages.com");
+		
+//		WebElement logInElement  = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[4]/ul/li[1]/a"));
+//		WebElement logInElement  = driver.findElement(By.xpath("//a[.='Log in']"));
+		WebElement logInElement  = driver.findElement(By.xpath("//a[text()='Log in']"));
+		
+		logInElement.click();
+		
+		Thread.sleep(3000);
+		
+		driver.navigate().back();
+		
+//		WebElement signUpElement  = driver.findElement(By.xpath("//a[text()='Sign up']"));
+		WebElement signUpElement  = driver.findElement(By.xpath("//a[contains(text(),'Sign up')]"));
+		
+		signUpElement.click();
+
+		driver.close();
+	}
+
+}
