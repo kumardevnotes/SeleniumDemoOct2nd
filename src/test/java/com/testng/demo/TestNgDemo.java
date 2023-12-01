ddc3563447de9c4b04b9b81b403ab98dbd16ee2f
kumardevnotes <kumar.thoudishetti@gmail.com>
2023-12-01 23:01:01 +0530

    added

---
+++ src/test/java/com/testng/demo/TestNgDemo.java
@@ -0,0 +1,16 @@
+package com.testng.demo;
+
+import org.testng.annotations.Test;
+
+public class TestNgDemo {
+	
+ @Test
+  public void firstTestCase() {
+	  System.out.println("first testcase passed");
+  }
+ 
+ @Test
+ public void secondTestCase() {
+	  System.out.println("second testcase passed");
+ }
+}
